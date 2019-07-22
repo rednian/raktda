a188b9eab210f0e2929aa4367cae2db05c31a72d
jishnu1rks <dondelrosario93@gmail.com>
2019-07-22 10:09:09 +0400

    change in artist permit model

--- app/ArtistPermit.php
+++ app/ArtistPermit.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+
 use DB;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
@@ -23,38 +24,33 @@ class ArtistPermit extends Model
         return $this->hasOne(Artist::class, 'artist_permit_id');
     }
 
-      public function scoperequestType($q, $type)
-      {
-        return $q->select('*', DB::raw('COUNT(artist_id) as artist_number'), DB::raw('artist_permit.created_at AS submitted_on') )
-                 ->join('artist', 'artist_permit.artist_permit_id', '=', 'artist.artist_permit_id')
-                 ->join('bls.company', 'bls.company.company_id', '=', 'artist_permit.company_id')
-                 ->where('artist_permit.permit_status', $type)
-                 ->groupBy('artist_permit.company_id');
-      }
+    public function scoperequestType($q, $type)
+    {
+        return $q->select('*', DB::raw('COUNT(artist_id) as artist_number'), DB::raw('artist_permit.created_at AS submitted_on'))
+            ->join('artist', 'artist_permit.artist_permit_id', '=', 'artist.artist_permit_id')
+            ->join('bls.company', 'bls.company.company_id', '=', 'artist_permit.company_id')
+            ->where('artist_permit.permit_status', $type)
+            ->groupBy('artist_permit.company_id');
+    }
 
 
-      public function scopeArtistPermit($query, $request)
-      {
+    public function scopeArtistPermit($query, $request)
+    {
         return $query->join('artist', 'artist_permit.artist_permit_id', '=', 'artist.artist_permit_id')
-                     ->join('bls.company', 'bls.company.company_id', '=', 'artist_permit.company_id')
-                     ->orderBy('artist_permit.company_id', 'DESC')  
-                    ->orderBy('artist_permit.created_at', 'DESC')
-                    ->where('permit_status', '!=', 'new')
-                    ->when($request->company_id, function($q) use ($request){
-                        $q->where('artist_permit.company_id', $request->company_id);
-                    });
-      }
-
-      public function artist()
-      {
-        return $this->hasMany(Artist::class, 'artist_permit_id');
-      }
+            ->join('bls.company', 'bls.company.company_id', '=', 'artist_permit.company_id')
+            ->orderBy('artist_permit.company_id', 'DESC')
+            ->orderBy('artist_permit.created_at', 'DESC')
+            ->where('permit_status', '!=', 'new')
+            ->when($request->company_id, function ($q) use ($request) {
+                $q->where('artist_permit.company_id', $request->company_id);
+            });
+    }
 
 
-      public function payment()
-      {
-        return $this->belongsToMany(PaymentTransaction::class, 'artist_payment', 'artist_permit_id','payment_id');
-      }
 
-  
+
+    public function payment()
+    {
+        return $this->belongsToMany(PaymentTransaction::class, 'artist_payment', 'artist_permit_id', 'payment_id');
+    }
 }
