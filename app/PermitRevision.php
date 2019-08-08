d6210b4c606257bb51df014a995a1e5d2ffef728
chrs olivo <rednianred@gmail.com>
2019-08-08 18:06:26 +0400

    changing revision models

--- app/PermitRevision.php
+++ app/PermitRevision.php
@@ -5,12 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
-class PermitRivision extends Model
+class PermitRevision extends Model
 {
       use SoftDeletes;
-      protected $table = 'permit_rivision';
-      protected $primaryKey = ['permit_rivision_id'];
-      protected $fillable = [ 'rivision__number', 'permit_id', 'artist_permit_comment_id'];
+      protected $table = 'permit_revision';
+      protected $primaryKey = ['permit_revision_id'];
+      protected $fillable = [ 'revision__number', 'permit_id', 'artist_permit_comment_id'];
 
       public function artistPermitComment()
       {
