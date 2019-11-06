ad56f2dffde6d08e247cd453e19cfadc8580ece5
chrs olivo <rednianred@gmail.com>
2019-11-06 11:01:11 +0400

    fixing

--- resources/views/admin/artist_permit/includes/new_request.blade.php
+++ resources/views/admin/artist_permit/includes/new_request.blade.php
@@ -1,22 +1,4 @@
 
-<section class="form-inline kt-padding-5 kt-margin-b-5" style="background:#f5f5f5">
-	 <label for="inlineFormInputName2" class="kt-margin-5 kt-font-dark"><span class="fa fa-filter kt-margin-r-5"></span> {{ __('Filter By:') }}</label>
-	 <select onchange="artistPermit.draw();" multiple="multiple" class=" mb-2 mr-sm-2 kt-margin-l-15" id="new-request-type">
-			<option value="new">New</option>
-			<option value="renew">Renew</option>
-			<option value="amend">Amend</option>
-	 </select>
-	 <label for="inlineFormInputName2" class="kt-margin-5"></label>
-	 <select onchange="artistPermit.draw();" multiple="multiple" class=" mb-2 mr-sm-2 kt-margin-l-15" id="new-permit-status">
-			<option value="new">New</option>
-			<option value="modified">Modified Permit by Client</option>
-			<option value="unprocessed">Unprocessed</option>
-	 </select>
-	 <label for="inlineFormInputName2"></label>
-	 {{-- <input type="text" id="new-applied-date" data-tab="new" class="form-control mb-2 mr-sm-2 kt-margin-l-15 kt-margin-t-5" placeholder="Start Date" autocomplete="off"> --}}
-</section>
-
-
 <section class="form-row">
 	<div class="col-1">
 		<div>
