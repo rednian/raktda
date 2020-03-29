a782ec6456f2f0f9f3efe7ed27114241051cd505
chris <rednianred@gmail.com>
2020-03-29 10:08:46 +0400

    some changes

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -110,7 +110,8 @@ class SettingController extends Controller
 		curl_setopt($curl, CURLOPT_URL, $url);
 		curl_setopt($curl, CURLOPT_HTTPHEADER, array(
 			'Content-Type: application/json'
-		));
+        ));
+
 		curl_setopt($curl, CURLOPT_USERPWD, $username . ":" . $password);
 		curl_setopt($curl, CURLOPT_POST, true);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($postFields));
