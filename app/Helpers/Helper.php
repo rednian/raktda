e66ec741ba620b6280b649429dab32f4a7e8dfd7
Jishnu Krishnan <jishnukrishnan30@gmail.com>
2020-04-29 16:35:17 +0400

    one updation in the translation file for establishment details(EN)

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -457,7 +457,7 @@ function getPaymentOrderId($from, $id)
     $payment_no = '';
     // dd($last_transaction);
     if (empty($last_transaction) || $last_transaction->payment_order_id == null) {
-        $payment_no = sprintf("%07d",  432);
+        $payment_no = sprintf("%07d",  320);
     } else {
         $last_trn = explode('-', $last_transaction->payment_order_id);
         $last_year = $last_trn[1];
